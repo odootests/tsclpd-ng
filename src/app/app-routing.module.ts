81101fb4812669748e1ba6441801fc58e8fa11dd
Toufiq <burningraven06@gmail.com>
2017-11-25 12:21:58 +0600

    nav added + works, routing added + works, boostrapped app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
-
 import { RouterModule, Routes } from '@angular/router';
+import { TscSec1Component }  from './tsc-sec1/tsc-sec1.component';
+
+const routes: Routes = [
+  { path: 'sec1', component: TscSec1Component }
+];
 
 @NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
   exports:[RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TscSec1Component } from './tsc-sec1/tsc-sec1.component';
+import { NavComponent} from './nav/nav.component';
 import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TscSec1Component
+    TscSec1Component,
+    NavComponent
   ],
   imports: [
     BrowserModule,
